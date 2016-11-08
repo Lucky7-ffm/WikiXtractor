28f51ced156d663b2592af1d61b6a1dc827b1178
xuiqzy <>
2016-11-08 13:22:22 +0100

    Add libraries and config for them, modify .gitignore for that, minor corrections

--- src/de/bened/wikixtractor/LinkExtractor.java
+++ src/de/bened/wikixtractor/LinkExtractor.java
@@ -2,11 +2,13 @@ package de.bened.wikixtractor;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.apache.log4j.Logger;
 
 
 /** Created by Symdox
@@ -14,16 +16,16 @@ import org.apache.log4j.Logger;
  */
 
 class LinkExtractor {
-    private final static Logger logger = Logger.getLogger(LinkExtractor.class);
+    private final static Logger logger = LogManager.getLogger(LinkExtractor.class);
 
     static Set<String> extractLinks(String pageHTML) {
         Set<String> linkSet = new HashSet<>();
         Document doc = Jsoup.parse(pageHTML);
 
-        //Looks for the HTML DIV called catlinks and take just that links with the beginning <a href...>
+        //Looks for the HTML DIV called catlinks and takes just that links with the beginning <a href...>
         Elements linkText = doc.select("div[id=catlinks] a[href]");
 
-        //Adds every Linktext from catlinks into a Set of Strings
+        //Adds every link text from catlinks into a Set of Strings
         for (Element link : linkText) {
             linkSet.add(link.text());
             logger.info("Successfully added " + link.text());
